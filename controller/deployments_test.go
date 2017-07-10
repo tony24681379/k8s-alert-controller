4cb521faeaf756a50bb04642272ccdae21c92fdd
Tony Huang <yellow24681379@gmail.com>
2017-07-10 16:16:51 +0800

    fix TestDeploymentUpdateReplicas

--- controller/deployments_test.go
+++ controller/deployments_test.go
@@ -38,24 +38,19 @@ func deployment() *v1beta1.Deployment {
 
 func TestDeploymentUpdateReplicas(t *testing.T) {
 	clientSet := fake.NewSimpleClientset(deployment())
-	_, err := clientSet.Extensions().Deployments(v1.NamespaceDefault).Get("foo", metav1.GetOptions{})
-	if err != nil {
-		t.Error(err)
-	}
-
 	deployment := DeploymentController{Deployment: clientSet.Extensions().Deployments(v1.NamespaceDefault)}
-	err = deployment.updateReplicas("foo", -1)
+	err := deployment.updateReplicas("foo", -1)
 
 	if err != nil {
 		t.Errorf("unexpected actions: %v", err)
 	}
 
 	actions := clientSet.Actions()
-	if len(actions) != 3 {
+	if len(actions) != 2 {
 		t.Errorf("unexpected actions: %+v, expected 2 actions (get, update)", actions)
 	}
 	expectReplicas := int32(2)
-	if action, ok := actions[2].(testcore.UpdateAction); !ok || action.GetResource().GroupResource() != v1beta1.Resource("deployments") || *(action.GetObject().(*v1beta1.Deployment).Spec.Replicas) != expectReplicas {
+	if action, ok := actions[1].(testcore.UpdateAction); !ok || action.GetResource().GroupResource() != v1beta1.Resource("deployments") || *(action.GetObject().(*v1beta1.Deployment).Spec.Replicas) != expectReplicas {
 		t.Errorf("unexpected action %v, expected update-deployment with replicas = %d", actions[1], expectReplicas)
 	}
 }
